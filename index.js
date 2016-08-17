011430c9845ce29c43c39d94a698a69b20a96809
JO <raphaeljose.balonga@gmail.com>
2016-08-17 10:04:33 +0800

    feat(doAsync): Problem number 1 - Asynchronous Operations

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ import { RandStream, asyncOp } from './lib/lib'
 //import "babel-polyfill"
 
 'use strict';
+
+
+/*
+ *
+ *  PROBLEM 1: ASYNCHRONOUS OPERATIONS
+ *
+ */
+
+function doAsync(input) {
+    input.map(x => {
+        if(Array.isArray(x))
+            x.map(y => asyncOp(y))
+        else
+            asyncOp(x)
+    });
+}
+
+let input = [
+  'A',
+  [ 'B', 'C', 'D', 'E' ],
+  'F',
+  'G',
+  [ 'H', 'I' ]
+]
+
+doAsync(input);
