420e370ddb251761cf806b408f45ec273c9aa368
JO <raphaeljose.balonga@gmail.com>
2016-08-17 10:27:46 +0800

    feat(RandStringSource): Problem 2 - Streams solution

--- index.js
+++ index.js
@@ -10,21 +10,64 @@ import { RandStream, asyncOp } from './lib/lib'
  *
  */
 
-function doAsync(input) {
-    input.map(x => {
-        if(Array.isArray(x))
-            x.map(y => asyncOp(y))
-        else
-            asyncOp(x)
+// function doAsync(input) {
+//     input.map(x => {
+//         if(Array.isArray(x))
+//             x.map(y => asyncOp(y))
+//         else
+//             asyncOp(x)
+//     });
+// }
+
+// let input = [
+//   'A',
+//   [ 'B', 'C', 'D', 'E' ],
+//   'F',
+//   'G',
+//   [ 'H', 'I' ]
+// ]
+
+// doAsync(input);
+
+
+// // /*
+// //  *
+// //  *  PROBLEM 2: STREAMS
+// //  *
+// //  */
+
+
+var util = require('util'),
+    events = require('events');
+
+function RandStringSource(stream) {
+    this.stream = stream;
+    let self = this;
+
+    let string = '';
+    events.EventEmitter.call(this);
+
+    stream.on('data', (data) => {
+        data.split('').forEach(function(c) {
+            if(c == '.') {
+                self.emit('data', string);
+                string = '';
+            }
+            else
+                string += c;
+        });
+
     });
+
 }
 
-let input = [
-  'A',
-  [ 'B', 'C', 'D', 'E' ],
-  'F',
-  'G',
-  [ 'H', 'I' ]
-]
+util.inherits(RandStringSource, events.EventEmitter);
 
-doAsync(input);
+let source = new RandStringSource(new RandStream());
+
+source.on('data', (data) => {
+    console.log(data);
+})
+
+
+
