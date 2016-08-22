c95a38d8ee52382fa5554c8203425fd63d59bcd5
JO <raphaeljose.balonga@gmail.com>
2016-08-22 16:33:06 +0800

    build(Resource Manager): Solution to problem 3 of the problem set Resource Pooling

--- index.js
+++ index.js
@@ -69,5 +69,80 @@ source.on('data', (data) => {
     console.log(data);
 })
 
+/*
+ *
+ *  PROBLEM 3: RESOURCE POOLING
+ *
+ */
+
+var advancedPool = require('advanced-pool');
+
+class ResourceManager {
+    constructor(count) {
+        this.count = count;
+        this.curr = 0;
+        this.resources = 1;
+        this.resource;
+        let self = this;
+        this.pull = new advancedPool.Pool({
+            min: this.count,
+            max: this.count,
+            create: function(callback) {
+                var resource = {name: "Resource #" + self.resources};
+                callback(null, resource);
+                self.resources++;
+            }
+        })
+    }
+
+    borrow(callback) {
+        this.curr++;
+        let pool = this.pull
+        , self = this;
+
+        let fn = function (err, resource) {
+            if (err) {
+                console.log(err);
+            } else {
+                self.resource = resource;
+                callback(self);
+            }
+        };
+        pool.acquire(fn.bind({ id: this.curr }));
+    }
+
+    release() {
+        let pool = this.pull;
+        pool.release(this.resource);
+    }
+
+    close() {
+        let pool = this.pull;
+        pool.close();
+    }
+}
+
+let pool = new ResourceManager(2);
+console.log('START');
+
+let timestamp = Date.now();
+
+pool.borrow((res) => {
+  console.log('RES: 1');
+
+  setTimeout(() => {
+    res.release();
+  }, 500);
+});
+
+pool.borrow((res) => {
+  console.log('RES: 2');
+});
+
+pool.borrow((res) => {
+  console.log('RES: 3');
+  console.log('DURATION: ' + (Date.now() - timestamp));
+});
+
 
 
--- index2.js
+++ index2.js
@@ -1,6 +1,10 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _lib = require('./lib/lib');
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 //import "babel-polyfill"
 
@@ -56,4 +60,88 @@ var source = new RandStringSource(new _lib.RandStream());
 
 source.on('data', function (data) {
     console.log(data);
+});
+
+/*
+ *
+ *  PROBLEM 3: RESOURCE POOLING
+ *
+ */
+
+var advancedPool = require('advanced-pool');
+
+var ResourceManager = function () {
+    function ResourceManager(count) {
+        _classCallCheck(this, ResourceManager);
+
+        this.count = count;
+        this.curr = 0;
+        this.resources = 1;
+        this.resource;
+        var self = this;
+        this.pull = new advancedPool.Pool({
+            min: this.count,
+            max: this.count,
+            create: function create(callback) {
+                var resource = { name: "Resource #" + self.resources };
+                callback(null, resource);
+                self.resources++;
+            }
+        });
+    }
+
+    _createClass(ResourceManager, [{
+        key: 'borrow',
+        value: function borrow(callback) {
+            this.curr++;
+            var pool = this.pull,
+                self = this;
+
+            var fn = function fn(err, resource) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    self.resource = resource;
+                    callback(self);
+                }
+            };
+            pool.acquire(fn.bind({ id: this.curr }));
+        }
+    }, {
+        key: 'release',
+        value: function release() {
+            var pool = this.pull;
+            pool.release(this.resource);
+        }
+    }, {
+        key: 'close',
+        value: function close() {
+            var pool = this.pull;
+            pool.close();
+        }
+    }]);
+
+    return ResourceManager;
+}();
+
+var pool = new ResourceManager(2);
+console.log('START');
+
+var timestamp = Date.now();
+
+pool.borrow(function (res) {
+    console.log('RES: 1');
+
+    setTimeout(function () {
+        res.release();
+    }, 500);
+});
+
+pool.borrow(function (res) {
+    console.log('RES: 2');
+});
+
+pool.borrow(function (res) {
+    console.log('RES: 3');
+    console.log('DURATION: ' + (Date.now() - timestamp));
 });
